2067ac2a0e97db113cb4eb5f65f32c3c59c2e4a5
Gustavo Luís Pandini <gugui.pandini@gmail.com>
2023-10-11 21:32:18 -0300

    Primiero Commit

---
+++ Integração do Visual Studio com MYSQL/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Integração_do_Visual_Studio_com_MYSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Integração do Visual Studio com MYSQL/UsuarioEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integração_do_Visual_Studio_com_MYSQL
+{
+    internal class UsuarioEntity
+    {
+    }
+}
