fba443795cfcbb8f052a51c42214d64da79722fd
Gustavo Luís Pandini <gugui.pandini@gmail.com>
2023-10-16 19:12:55 -0300

    Concluida o ColetorEntity

---
+++ Integração do Visual Studio com MYSQL/ColetorEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integração_do_Visual_Studio_com_MYSQL
+{
+    public class ColetorEntity
+    {
+        public int idColetor { get; set; }
+        public string CPF { get; set; }
+        public string CNPJ { get; set; }
+        public string Endereco { get; set; }
+        public string Coleta { get; set; }
+        public string Usuario_idUsuario { get; set; }
+    }
+}
