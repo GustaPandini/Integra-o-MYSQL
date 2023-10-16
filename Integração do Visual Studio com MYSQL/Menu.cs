a6e1ab782f04cb81f5dd0595d482cc11f7e89f7f
Gustavo Luís Pandini <gugui.pandini@gmail.com>
2023-10-16 19:44:32 -0300

    Criado o Cadastromodel e o Menu

---
+++ Integração do Visual Studio com MYSQL/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integração_do_Visual_Studio_com_MYSQL
+{
+    public class Menu
+    {
+        private CadastroModel _cadastroModel = new CadastroModel();
+        public int LegendaMenuPrincipal()
+        {
+            Console.WriteLine("1 = Cadastrar Usuario ou Coletor");
+            Console.WriteLine("2 = Mostrar Usuarios e Coletores");
+            Console.WriteLine("3 = Atualizar Cadastro de Usuarios ou Coletores");
+            Console.WriteLine("4 = Excluir Cadastro");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public void MenuPrincipal()
+        {
+            switch (LegendaMenuPrincipal())
+            {
+                case 0:
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+    }
+}
--- Integração do Visual Studio com MYSQL/Program.cs
+++ Integração do Visual Studio com MYSQL/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            //Vazio
+            Menu menu = new Menu();
+            menu.MenuPrincipal();
         }
     }
 }
