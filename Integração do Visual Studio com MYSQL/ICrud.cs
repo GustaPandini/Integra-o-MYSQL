b713aefe6608a0047c6216d88f6233dbd7895560
Gustavo Luís Pandini <gugui.pandini@gmail.com>
2023-10-16 19:17:01 -0300

    Criado a Interface ICrud

---
+++ Integração do Visual Studio com MYSQL/ICrud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integração_do_Visual_Studio_com_MYSQL
+{
+    public interface ICrud
+    {
+        public void Create();
+        public void Read();
+        public void Update();
+        public void Delete();
+    }
+}
