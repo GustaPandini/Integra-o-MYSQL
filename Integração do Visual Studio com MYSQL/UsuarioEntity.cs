e0d510f43d4ae4cf8ec56def31afa3737733bb60
Gustavo Luís Pandini <gugui.pandini@gmail.com>
2023-10-16 19:04:16 -0300

    Corigindo erro do Commit

--- Integração do Visual Studio com MYSQL/UsuarioEntity.cs
+++ Integração do Visual Studio com MYSQL/UsuarioEntity.cs
@@ -5,9 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Integração_do_Visual_Studio_com_MYSQL
-{
-    internal class UsuarioEntity
+{   
+    public class UsuarioEntity
     {
-        //ola
+        public int idUsuario { get; set; }
+        public string NOME { get; set; }
+        public string Sexo { get; set; }
+        public int Idade { get; set; }
+        public string Telefone { get; set; }
     }
 }
