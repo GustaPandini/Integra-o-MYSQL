940514f3f11b0203ea69daaa3980d597ec206e61
Gustavo Luís Pandini <gugui.pandini@gmail.com>
2023-10-18 19:49:19 -0300

    Finalizado cadastro coletor

--- Integração do Visual Studio com MYSQL/CadastroModel.cs
+++ Integração do Visual Studio com MYSQL/CadastroModel.cs
@@ -16,12 +16,48 @@ namespace Integração_do_Visual_Studio_com_MYSQL
             UsuarioEntity cadastro = new UsuarioEntity();
             Console.WriteLine("Digite o seu Nome");
             cadastro.NOME = Console.ReadLine();
+            Console.WriteLine("Digite o seu Sexo");
+            cadastro.Sexo = Console.ReadLine();
+            Console.WriteLine("Digite a sua Idade");
+            cadastro.Idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o seu Telfone");
+            cadastro.Telefone = Console.ReadLine();
 
             using (MySqlConnection connection = new MySqlConnection(conectionString))
             {
                 string sql = "INSERT INTO usuario VALUE (NULL, @NOME, @Sexo, @Idade, @Telefone)";
                 int linhas = connection.Execute(sql, cadastro);
                 Console.WriteLine($"Tipo inserido - {linhas} linhas afetadas");
+            }
+
+            Console.WriteLine("Deseja ser um Coletor de Residuos? ( S/N )");
+            string resp1 = Console.ReadLine().ToUpper();
+            if (resp1 == "S")
+            {
+                ColetorEntity coletor = new ColetorEntity();
+                Console.WriteLine("O seu tipo de pessoa é Física (F) ou Juridica (J)?");
+                string resp2 = Console.ReadLine().ToUpper();
+                if (resp2 == "F")
+                {
+                    Console.WriteLine("Digite seu CPF");
+                    coletor.CPF = Console.ReadLine();
+                }
+                if (resp2 == "J")
+                {
+                    Console.WriteLine("Digite seu CNPJ");
+                    coletor.CNPJ = Console.ReadLine();
+                }
+                Console.WriteLine("Digite o seu Endereco");
+                coletor.Endereco = Console.ReadLine();
+                Console.WriteLine("Digite a sua forma de Coleta");
+                coletor.Coleta = Console.ReadLine();
+
+                using (MySqlConnection connection = new MySqlConnection(conectionString))
+                {
+                    string sql = "INSERT INTO coletor VALUE (NULL, @CPF, @CNPJ, @Endereco, @Coleta)";
+                    int linhas = connection.Execute(sql, coletor);
+                    Console.WriteLine($"Tipo inserido - {linhas} linhas afetadas");
+                }
             }
         }
 
