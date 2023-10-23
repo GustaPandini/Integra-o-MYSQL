9b4f64e0c51b1eb8846e2096bbb90ee23637f07e
Gustavo Luís Pandini <gugui.pandini@gmail.com>
2023-10-23 19:14:53 -0300

    Create do Cadastro Organizada

--- Integração do Visual Studio com MYSQL/CadastroModel.cs
+++ Integração do Visual Studio com MYSQL/CadastroModel.cs
@@ -2,6 +2,7 @@
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,34 +21,68 @@ namespace Integração_do_Visual_Studio_com_MYSQL
             if (resp == "S")
             {
                 ColetorEntity coletor = new ColetorEntity();
-                string resp2 = Pergunta_S_N("Pessoa Física (F) ou Juridica (J)?");
-                if (resp2 == "F")
-                {
-                    Console.WriteLine("Digite seu CPF");
-                    coletor.CPF = Console.ReadLine();
-                }
-                if (resp2 == "J")
-                {
-                    Console.WriteLine("Digite seu CNPJ");
-                    coletor.CNPJ = Console.ReadLine();
-                }
-                Console.WriteLine("Digite o seu Endereco");
-                coletor.Endereco = Console.ReadLine();
-                Console.WriteLine("Digite a sua forma de Coleta");
-                coletor.Coleta = Console.ReadLine();
-                using (MySqlConnection connection = new MySqlConnection(conectionString))
-                {
-                    string sql = "SELECT * FROM usuario ORDER BY idUsuario DESC LIMIT 1";
-                    coletor.Usuario_idUsuario = this.GetConnection().QueryFirst<UsuarioEntity>(sql).idUsuario;
-                }
-
-                using (MySqlConnection connection = new MySqlConnection(conectionString))
-                {
-                    string sql = "INSERT INTO coletor VALUE (NULL, @CPF, @CNPJ, @Endereco, @Coleta, @Usuario_idUsuario)";
-                    int linhas = connection.Execute(sql, coletor);
-                    Console.WriteLine($"Tipo inserido - {linhas} linhas afetadas");
-                }
+                CadastrarNovoColetor(coletor);
             }
+        }
+
+        private void CadastrarNovoColetor(ColetorEntity coletor)
+        {
+            string resp3 = Pergunta_S_N("Pessoa Física (F) ou Juridica (J)?");
+            if (resp3 == "F")
+            {
+                CadastrarCPF(coletor);
+            }
+            if (resp3 == "J")
+            {
+                CadastrarCNPJ(coletor);
+            }
+            CadastrarEndereco(coletor);
+            CadastrarFormaColeta(coletor);
+            PegarUltimoIDUsuario(coletor);
+            PopularColetorNoBanco(coletor);
+        }
+
+        private void PopularColetorNoBanco(ColetorEntity coletor)
+        {
+            using (MySqlConnection connection = new MySqlConnection(conectionString))
+            {
+                string sql = "INSERT INTO coletor VALUE (NULL, @CPF, @CNPJ, @Endereco, @Coleta, @Usuario_idUsuario)";
+                int linhas = connection.Execute(sql, coletor);
+                Console.WriteLine($"Tipo inserido - {linhas} linhas afetadas");
+            }
+        }
+
+        private void PegarUltimoIDUsuario(ColetorEntity coletor)
+        {
+            using (MySqlConnection connection = new MySqlConnection(conectionString))
+            {
+                string sql = "SELECT * FROM usuario ORDER BY idUsuario DESC LIMIT 1";
+                coletor.Usuario_idUsuario = this.GetConnection().QueryFirst<UsuarioEntity>(sql).idUsuario;
+            }
+        }
+
+        private static void CadastrarFormaColeta(ColetorEntity coletor)
+        {
+            Console.WriteLine("Digite a sua forma de Coleta");
+            coletor.Coleta = Console.ReadLine();
+        }
+
+        private static void CadastrarEndereco(ColetorEntity coletor)
+        {
+            Console.WriteLine("Digite o seu Endereco");
+            coletor.Endereco = Console.ReadLine();
+        }
+
+        private void CadastrarCNPJ(ColetorEntity coletor)
+        {
+            Console.WriteLine("Digite seu CNPJ");
+            coletor.CNPJ = Console.ReadLine();
+        }
+
+        private void CadastrarCPF(ColetorEntity coletor)
+        {
+            Console.WriteLine("Digite seu CPF");
+            coletor.CPF = Console.ReadLine();
         }
 
         private static string Pergunta_S_N(string mensagem)
